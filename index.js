7529fb23efd6531da75f423f54cdafdf0e6980c7
S H-SuHan <suhanur35-448@diu.edu.bd>
2023-10-19 05:46:20 +0600

    Single Product items get here

--- index.js
+++ index.js
@@ -37,12 +37,12 @@ async function run() {
             res.send(result)
         })
 
-        // app.get("/product/:id", async (req, res) => {
-        //     const id = req.params.id
-        //     const query = {_id: new ObjectId(id)}
-        //     const result = await productCollection.findOne(query)
-        //     res.send(result)
-        // })
+        app.get("/product/:id", async (req, res) => {
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result = await productCollection.findOne(query)
+            res.send(result)
+        })
 
         // app.post('/product', async (req, res) =>{
         //     const product = req.body;
